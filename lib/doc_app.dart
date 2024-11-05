b50014e7a836d1192e203594b2a09afa731a7f7e
Noha0212 <nonathabit2002@gmail.com>
2024-11-05 18:35:26 +0200

    ScreenUtilInit and theme at doc app

---
+++ lib/doc_app.dart
@@ -0,0 +1,32 @@
+import 'package:doc_doc/core/routing/app_router.dart';
+import 'package:doc_doc/core/routing/routes.dart';
+import 'package:doc_doc/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({
+    super.key,
+    required this.appRouter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.primaryBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        debugShowCheckedModeBanner: false,
+        //builder: DevicePreview.appBuilder,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
+import 'package:doc_doc/core/routing/app_router.dart';
+import 'package:doc_doc/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DocApp());
-}
-
-class DocApp extends StatelessWidget {
-  const DocApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
