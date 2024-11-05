3bc53b11f4cbe22e9a94323f2271053bc0aa2a03
Noha0212 <nonathabit2002@gmail.com>
2024-11-05 17:59:24 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DocApp());
+}
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
